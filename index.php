89ee4a0606009ac465925a39809c27abf8c3c90f
Henrique Passarelli <henrique.passarellicc@gmail.com>
2022-04-09 17:37:41 -0300

    favicon and title

--- index.php
+++ index.php
@@ -6,14 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Calculator Imc</title>
+    <title>calculadora IMC</title>
 </head>
 
 <body>
     <div class="card d-flex flex-column gap-3 justify-content-center align-items-center p-5">
-        <h1>Calculator</h1>
+        <h1>Calculadora IMC</h1>
         <a href="https://github.com/HenriquePassarelli">
             <svg height="32" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="32" data-view-component="true"
                 class="octicon octicon-mark-github v-align-middle">
